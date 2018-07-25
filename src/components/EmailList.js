82e74d9c8bbab179db3e71836d9ff2b7a73e4ba8
kravitzluis <kravitzluis@gmail.com>
2018-07-25 05:24:10 -0500

    Modificate structure of EmailList

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -8,10 +8,26 @@ class EmailList extends Component {
           <h1 className="List-title">Header</h1>
         </header>
         <div className="List-custom">
-        <div className="item-custom">number 1</div>
-        <div className="item-custom">number 2</div>
-        <div className="item-custom">number 3</div>
-        <div className="item-custom">number 4</div>
+        <div className="item-custom">
+          <div className="item-user"><div className="user-icon"></div> <div className="user-name">Google</div><div className="item-date">Jun 1</div></div>
+          <div className="item-tittle">Amazon SESS Addres Verification Request</div>
+          <div className="item-description">Lorem ipsum dolor amet consecutire dv </div>
+        </div>
+        <div className="item-custom">
+          <div className="item-user"><div className="user-icon"></div> <div className="user-name">Amazon</div><div className="item-date">May 23</div></div>
+          <div className="item-tittle">Amazon SESS Addres Verification Request</div>
+          <div className="item-description">Lorem ipsum dolor amet consecutire dv </div>
+        </div>
+        <div className="item-custom">
+          <div className="item-user"><div className="user-icon"></div> <div className="user-name">Microsoft</div><div className="item-date">Abr 7</div></div>
+          <div className="item-tittle">Amazon SESS Addres Verification Request</div>
+          <div className="item-description">Lorem ipsum dolor amet consecutire dv </div>
+        </div>
+        <div className="item-custom">
+          <div className="item-user"><div className="user-icon"></div> <div className="user-name">Github</div><div className="item-date">Feb 11</div></div>
+          <div className="item-tittle">Amazon SESS Addres Verification Request</div>
+          <div className="item-description">Lorem ipsum dolor amet consecutire dv </div>
+        </div>
         </div>
       </div>
     );
