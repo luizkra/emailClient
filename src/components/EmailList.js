27b9342684d62a2b38e1ac46a4c315f3b7d47afa
Carlos Campos <carloscampos@Macs-Air.lan>
2018-07-25 18:48:48 -0500

    Ajust column of emails

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -9,24 +9,32 @@ class EmailList extends Component {
         </header>
         <div className="List-custom">
         <div className="item-custom">
+        <div className="item-wrapper">
           <div className="item-user"><div className="user-icon"></div> <div className="user-name">Google</div><div className="item-date">Jun 1</div></div>
           <div className="item-tittle">Amazon SESS Addres Verification Request</div>
           <div className="item-description">Lorem ipsum dolor amet consecutire dv </div>
         </div>
+        </div>
         <div className="item-custom">
-          <div className="item-user"><div className="user-icon"></div> <div className="user-name">Amazon</div><div className="item-date">May 23</div></div>
+        <div className="item-wrapper">
+          <div className="item-user"><div className="user-icon"></div> <div className="user-name">Google</div><div className="item-date">Jun 1</div></div>
           <div className="item-tittle">Amazon SESS Addres Verification Request</div>
           <div className="item-description">Lorem ipsum dolor amet consecutire dv </div>
         </div>
+        </div>
         <div className="item-custom">
-          <div className="item-user"><div className="user-icon"></div> <div className="user-name">Microsoft</div><div className="item-date">Abr 7</div></div>
+        <div className="item-wrapper">
+          <div className="item-user"><div className="user-icon"></div> <div className="user-name">Google</div><div className="item-date">Jun 1</div></div>
           <div className="item-tittle">Amazon SESS Addres Verification Request</div>
           <div className="item-description">Lorem ipsum dolor amet consecutire dv </div>
         </div>
+        </div>
         <div className="item-custom">
-          <div className="item-user"><div className="user-icon"></div> <div className="user-name">Github</div><div className="item-date">Feb 11</div></div>
+        <div className="item-wrapper">
+          <div className="item-user"><div className="user-icon"></div> <div className="user-name">Google</div><div className="item-date">Jun 1</div></div>
           <div className="item-tittle">Amazon SESS Addres Verification Request</div>
           <div className="item-description">Lorem ipsum dolor amet consecutire dv </div>
+        </div>
         </div>
         </div>
       </div>
