e6e12177c53a4747abbae8f9b958262da6f96100
kravitzluis <kravitzluis@gmail.com>
2018-07-26 01:21:51 -0500

    Add mail-data.json and import in App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './css/App.css';
 import Content from './components/Content';
 import EmailList from './components/EmailList';
+import data from './data/mail-data.json';
 class App extends Component {
 
   render() {
     return (
       <div className="App-container">
         <div className="App-left-side">
-          <EmailList />
+          <EmailList items={data} />
         </div>
         <div className="App-right-side">
           <Content />
