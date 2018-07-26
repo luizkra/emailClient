0095c15615d28ff80a3bf650cee7341e5f76ed46
kravitzluis <kravitzluis@gmail.com>
2018-07-26 02:45:21 -0500

    Add email in intervaltime

--- src/App.js
+++ src/App.js
@@ -4,12 +4,27 @@ import Content from './components/Content';
 import EmailList from './components/EmailList';
 import data from './data/mail-data.json';
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      mails: []
+    }
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      data.map((mails, index)=>{
+        console.log(mails[index], index)
+        var newMails = this.state.mails.concat(mails);
+        this.setState({ mails: newMails })
+      })
+    }, 9000);
+  }
 
   render() {
     return (
       <div className="App-container">
         <div className="App-left-side">
-          <EmailList items={data} />
+          <EmailList items={this.state.mails} />
         </div>
         <div className="App-right-side">
           <Content />
