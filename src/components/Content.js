c27dde87a0178e001db8d1f310383c30b4fadaa9
kravitzluis <kravitzluis@gmail.com>
2018-07-27 05:24:38 -0500

    Feature to show email detail, and set read

--- src/components/Content.js
+++ src/components/Content.js
@@ -10,22 +10,77 @@ class Content extends Component {
     }
     store.subscribe(() => {
       this.setState({
-        mailViewer: store.getState().mailViewer
+        mailViewer: store.getState().mailViewer,
+        trashMails: store.getState().trashMails,
+        spanMails: store.getState().spanMails
       })
     })
   }
   render() {
     return (
-      <div className="Content"> 
-      {this.state.mailViewer.map((item) => 
-        <div>
+      <ContentMail verifyContent={this.state.mailViewer}/>
+    );
+  }
+
+  addSpam(idMail) {
+    store.dispatch({
+      type: "ADD_TO_SPAM",
+      idMail
+    })
+  }
+  addTrash(idMail) {
+    store.dispatch({
+      type: "ADD_TO_TRASH",
+      idMail
+    })
+  }
+  viewEmail(idMail) {
+    store.dispatch({
+      type: "VIEW_EMAIL",
+      idMail
+    })
+  }
+}
+
+function ContentMail(props) {
+    const lenghtArray = props.verifyContent;
+    if (lenghtArray.length > 0) {
+      return <ContendFull arrayContent={lenghtArray}/>;
+    }
+    return <EmptyContend />;
+  }
+
+  function EmptyContend() {
+    return (
+          <div className="Content-empty">
+              <i className="far fa-envelope mail-icon"></i>
+          </div>
+    );
+  }
+function ContendFull(props) {
+  const arrayContent_ = props.arrayContent;
+  return (
+    <div className="Content">
+      {arrayContent_.map((item, index) => 
+        <div className="Wrapper-content"key={index}>
         <header className="Content-header">
+          <div className="left-menu">
           <h5 className="Content-title">{item.subject}</h5>
+          </div>
+          <div className="rigth-menu">
+            <i className="fas fa-circle icon-action"></i>
+            <i className="far fa-trash-alt icon-action"></i>
+            <i className="fas fa-microchip icon-action"></i>
+          </div>
         </header>
         <div className="Content-container">
           <div className="Mail-header">
             <div className="Mail-from">{item.from}</div>
-            <div className="Mail-date">{item.date}</div>
+            <div className="Mail-date">{new Intl.DateTimeFormat('es-GB', { 
+                  year: 'numeric', 
+                  month: 'long', 
+                  day: '2-digit' 
+                }).format(new Date(item.date))}</div>
           </div>
           <div className="Mail-text">{item.body}
           </div>
@@ -33,7 +88,6 @@ class Content extends Component {
         </div>
         )}
       </div>
-    );
-  }
+  );
 }
 export default Content;
--- src/lib/store.js
+++ src/lib/store.js
@@ -1,20 +1,21 @@
 import { createStore} from 'redux'
-
 const reducer = (state, action) => {
-	
+	const arrayGlobal = state.listMails
 	switch(action.type) {
     case 'ADD_TO_LIST':
+    const arrayAux1 = arrayGlobal.concat(action.mail)
       return {
       	...state,
-      	listMails: state.listMails.concat(action.mail)
+      	listMails: arrayAux1
       }
     case 'ADD_TO_VIEWER':
+      //const arrayAux =arrayGlobal.map((mail, index) => { (index == action.idMail)?(console.log(mail), mail.isReaded=true):(console.log('not found ', index,action.idMail)); return mail})
       return {
       	...state,
-      	mailViewer: state.listMails.map((mail, index) => {mail.index == action.idMail, console.log(index)})
+      	mailViewer: arrayGlobal.filter((mail, index) => {return index == action.idMail})
       }
     default:
       return state
   	}
 }
-export default createStore(reducer, { listMails: [], mailViewer: [] })
+export default createStore(reducer, { listMails: [],spanMails: [],trashMails: [], mailViewer: [] })
