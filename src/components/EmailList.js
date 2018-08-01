85e167f47fe4793bdd28942269fbc367ea400098
luizkra <kravitzluis@gmail.com>
2018-08-01 02:21:35 -0500

    Configure react redux on Content and EmailList

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -1,179 +1,81 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import store from '../lib/store';
 import '../scss/EmailList.scss';
 import { addToViewer, showImbox, showTrash, showSpam } from '../lib/actionCreators';
+import { connect } from 'react-redux';
+import Selector from './Selector';
 
-class EmailList extends Component {
-  constructor() {
-    super();
-
-    this.addToViewer = this.addToViewer.bind(this);
-    this.showImbox = this.showImbox.bind(this);
-    this.showTrash = this.showTrash.bind(this);
-    this.showSpam = this.showSpam.bind(this);
-
-    this.state = {
-      listMails: [],
-      trashMails: [],
-      spanMails: [],
-      current: [],
-      currentView: -1
-    }
-
-    store.subscribe(() => {
-      this.setState({
-        listMails: store.getState().listMails,
-        trashMails: store.getState().trashMails,
-        spanMails: store.getState().spanMails,
-        current: store.getState().current,
-        currentView: store.getState().currentView
-      })
-    })
-  }
-
-  static propTypes = {
-    title: PropTypes.string,
-    items: PropTypes.array
-  };
-
-  viewEmail = (index) => {
-    console.log('Show index ', index)
-  }
-
-  render() {
-    const { title, items } = this.props;
-    let showList = "";
-    let ContentImbox = (
-      <div className={(this.state.listMails.length < 1) ? 'text-center-flex' : 'content-list'}>
-      {this.state.listMails.length < 1 &&
-          <div className="text-center-flex">
-            <h2 className="text-empty"> Bandeja de entrada vacía</h2>
-          </div>
-        }
-        {this.state.listMails.length > 0 &&
-        <div className="List-custom">
-          {this.state.listMails.map((item, index) => 
-            <div className={(item.isReaded == true && this.state.current.idMail != index) ? 'item-custom' : (this.state.current.idMail === index && item.isReaded == true) ? 'item-custom active' : 'item-custom new'} key={index} onClick={() => this.addToViewer(index, 'imbox')}>
-              <div className="item-wrapper">
-                <div className="item-user">
-                  <div className="user-icon"/>
-                  <div className="user-name">{item.from}</div>
-                  <div className="item-date">{new Intl.DateTimeFormat('en-GB', { 
-                    year: 'numeric', 
-                    month: 'long', 
-                    day: '2-digit' 
-                  }).format(new Date(item.date))}</div>
-                </div>
-                <div className="item-tittle">{item.subject}</div>
-                <div className="item-description">{item.body}</div>
-              </div>
-            </div>
-          )}
-        </div>
-        }
-      </div>
-      );
-    let ContentTrash = (
-      <div className={(this.state.trashMails.length < 1) ? 'text-center-flex' : 'content-list'}>
-      {this.state.trashMails.length < 1 &&
-          <div className="text-center-flex">
-            <h2 className="text-empty"> Correo basura vacío</h2>
-          </div>
-        }
-        {this.state.trashMails.length > 0 &&
-        <div className="List-custom">
-          {this.state.trashMails.map((item, index) => 
-            <div className={(item.isReaded == true && this.state.current.idMail != index) ? 'item-custom' : (this.state.current.idMail === index && item.isReaded == true) ? 'item-custom active' : 'item-custom new'} key={index} onClick={() => this.addToViewer(index, 'trash')}>
-              <div className="item-wrapper">
-                <div className="item-user">
-                  <div className="user-icon"/>
-                  <div className="user-name">{item.from}</div>
-                  <div className="item-date">{new Intl.DateTimeFormat('en-GB', { 
-                    year: 'numeric', 
-                    month: 'long', 
-                    day: '2-digit' 
-                  }).format(new Date(item.date))}</div>
-                </div>
-                <div className="item-tittle">{item.subject}</div>
-                <div className="item-description">{item.body}</div>
-              </div>
-            </div>
-          )}
-        </div>
-        }
-      </div>
-      );
-    let ContentSpam = (
-      <div className={(this.state.spanMails.length < 1) ? 'text-center-flex' : 'content-list'}>
-      {this.state.spanMails.length < 1 &&
-          <div className="text-center-flex">
-            <h2 className="text-empty"> Spam vacío</h2>
-          </div>
-        }
-        {this.state.spanMails.length > 0 &&
-        <div className="List-custom">
-          {this.state.spanMails.map((item, index) => 
-            <div className={(item.isReaded == true && this.state.current.idMail != index) ? 'item-custom' : (this.state.current.idMail === index && item.isReaded == true) ? 'item-custom active' : 'item-custom new'} key={index} onClick={() => this.addToViewer(index, 'spam')}>
-              <div className="item-wrapper">
-                <div className="item-user">
-                  <div className="user-icon"/>
-                  <div className="user-name">{item.from}</div>
-                  <div className="item-date">{new Intl.DateTimeFormat('en-GB', { 
-                    year: 'numeric', 
-                    month: 'short', 
-                    day: '2-digit' 
-                  }).format(new Date(item.date))}</div>
-                </div>
-                <div className="item-tittle">{item.subject}</div>
-                <div className="item-description">{item.body}</div>
-              </div>
-            </div>
-          )}
-        </div>
-        }
-      </div>
-      );
-    let imboxClass = ["content-icon"];
-    let trashClass = ["content-icon"];
-    let spamClass = ["content-icon"];
-    (this.state.currentView == 1)? (showList = ContentTrash, trashClass.push('active')) : (this.state.currentView == 2)?(showList = ContentSpam, spamClass.push('active')):(showList = ContentImbox, imboxClass.push('active'));
-
+const EmailList = ({ listMails, addToViewer, current }) => {
     return (
       <div className="List-container">
         <header className="List-header">
           <div className="left-menu-list">
-          <h3 className="List-title">{title}</h3>
+            <h3 className="List-title">title</h3>
           </div>
           <div className="rigth-menu-list">
-          <div className={imboxClass.join(' ')}>
-            <i className="fas fa-inbox icon-mail-list" onClick={() => this.showImbox()}> Inbox</i>
-          </div>
-          <div className={trashClass.join(' ')}>
-            <i className="far fa-trash-alt icon-mail-list" onClick={() => this.showTrash()}> Trash</i>
-          </div>
-          <div className={spamClass.join(' ')}>
-            <i className="fas fa-microchip icon-mail-list" onClick={() => this.showSpam()}> Spam</i>
-          </div>
+          <Selector/>
           </div>
         </header>
-        {showList}
+        <div className={(listMails.length < 1) ? 'text-center-flex' : 'content-list'}>
+          {listMails.length < 1 &&
+            <div className="text-center-flex">
+              <h2 className="text-empty">  vacío</h2>
+            </div>
+          }
+          {listMails.length > 0 &&
+            <div className="List-custom">
+              {listMails.map((item, index) =>
+                <div className={(item.isReaded == true && current.idMail != index) ? 'item-custom' : (current.idMail === index && item.isReaded == true) ? 'item-custom active' : 'item-custom new'} key={index} onClick={() => addToViewer(index, 'spam')}>
+                  <div className="item-wrapper">
+                    <div className="item-user">
+                      <div className="user-name">{item.from}</div>
+                      <div className="item-date">{new Intl.DateTimeFormat('en-GB', {
+                        year: 'numeric',
+                        month: 'short',
+                        day: '2-digit'
+                      }).format(new Date(item.date))}</div>
+                    </div>
+                    <div className="item-tittle">{item.subject}</div>
+                    <div className="item-description">{item.body}</div>
+                  </div>
+                </div>
+              )}
+            </div>
+          }
+        </div>
       </div>
     );
-  }
+  //}
 
-  addToViewer(idMail, typeMail) {
-    store.dispatch(addToViewer(idMail, typeMail))
-  }
-  showImbox() {
-    store.dispatch(showImbox())
-  }
-  showTrash() {
-    store.dispatch(showTrash())
-  }
-  showSpam() {
-    store.dispatch(showSpam())
-  }
+  // addToViewer(idMail, typeMail) {
+  //   store.dispatch(addToViewer(idMail, typeMail))
+  // }
+  // showImbox() {
+  //   store.dispatch(showImbox())
+  // }
+  // showTrash() {
+  //   store.dispatch(showTrash())
+  // }
+  // showSpam() {
+  //   store.dispatch(showSpam())
+  // }
 
 }
-export default EmailList;
+
+const mapStateToProps = state => {
+  console.log('​state lista', state);
+  return {
+    listMails: state.listMails,
+    current: state.current,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addToViewer(idMail, typeMail) {
+        dispatch(addToViewer(idMail, typeMail))
+    }
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(EmailList);
