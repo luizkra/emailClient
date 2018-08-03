f2732ab6aa991ee4068f1089f5aa29799dff0ecb
luizkra <kravitzluis@gmail.com>
2018-08-03 05:51:35 -0500

    Add component SearchCustom

--- src/components/SearchCustom.js
+++ src/components/SearchCustom.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Component } from "react";
+import { searchList } from "../lib/actionCreators";
+import store from "../lib/store";
 
-function SearchCustom(props) {
-  const { customClass, filterList, click } = props;
-  return (
+class SearchCustom extends Component {
+  constructor(props) {
+    super(props);
+    this.searchList = this.searchList.bind(this);
+  }
+  onChange(e) {
+    console.log("​SearchCustom -> onChange -> e", e.target.value);
+    // if(e.target.value.length > 0){
+    this.searchList(e.target.value);
+    // }
+  }
+
+  render() {
+    return (
       <div className="search-list">
-          <form>
-              <fieldset className="form-group">
-                  <input type="text" className="form-search" placeholder="Search" onChange={console.log('hola')} />
-              </fieldset>
-          </form>
+        <form>
+          <fieldset className="form-group">
+            <input
+              type="text"
+              className="form-search"
+              placeholder="Search"
+              onChange={e => this.onChange(e)}
+            />
+          </fieldset>
+        </form>
       </div>
-  )
+    );
+  }
+  searchList(text) {
+    store.dispatch(searchList(text));
+  }
 }
-
 export default SearchCustom;
